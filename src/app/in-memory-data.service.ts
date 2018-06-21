c83ef007e3ce07f378024822c9faabfa3a931906
Maria Elvira <mariaelvira.monserratvidal24@gmail.com>
2018-06-21 20:32:35 -0300

    Agregado de Info a Base
    -Se agrega info a la base de datos (in memory DB, productos.ts)
    
    -Se edita la vista para que muestre la información agregada (producto-detail)
    
    -Se Cambian algunas frases a español (producto-search y mesages)

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,16 +3,14 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const productos = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Miel de Azahar', details: 'De color ámbar claro, perfumada y suave al paladar. Procedente del polen de limoneros, naranjos, mandarinos. Efectos sedantes, ayudando conciliar el sueño. Es también antiespasmódica.', price: 125 },
+      { id: 12, name: 'Miel de Romero', details: 'De color ámbar muy claro y blanco cuando está cristalizada. Aromática y dulce. Estimulante hepático que favorece la descongestión del hígado. Indicada para las úlceras de estómago y dismenorrea. No recomendada a hipertensos.', price: 125  },
+      { id: 13, name: 'Miel de Eucaliptus', details: 'De color ocre. Muy aromática con un sabor característico a madera. Antiséptico de las vías respiratorias y urinarias, de efectos balsámicos y vermífugos. Indicada contra catarros y afecciones del árbol respiratorio.', price: 178  },
+      { id: 14, name: 'Miel de Tomillo', details: 'De tono rojizo y sabor agradable. Indicada para las afecciones respiratorias de tipo inflamatorio, tos convulsiva y asma. Reguladora de la tensión arterial. Muy indicada como tonificante ante la fatiga y la astenia.', price: 136  },
+      { id: 15, name: 'Miel de Espliego', details: 'De color ámbar y sabor característico a lavanda. Por su poder bactericida y antiséptico. Indicada en uso externo para picaduras de insectos, quemaduras solares y heridas. Antidiarreico. Preventivo de gripes, bronquitis, resfriados. Por su alto contenido en hierro, tiene las mismas indicaciones que la miel de castaño. Favorece el sueño, modera la irritabilidad, la agresividad y el estrés.', price: 174  },
+      { id: 16, name: 'Miel de Tilo', details: 'Procedente de las flores del tilo. De color amarillo suave con un aroma fragante. Se cristaliza fácilmente por lo que se endurece deprisa. Útil en la acidez, flatulencias, ulceras gástricas o duodenales. Propiedades antibacterianas. Útil en la bronquitis, gripe y resfriados. Posee propiedades sedantes siendo muy útil en ansiedad e insomnio.', price: 124  },
+      { id: 17, name: 'Miel de Trebol', details: 'Procedente de la flor de trébol De color amarillo suave. Es energetizante. Muy útil para cansancio o fatiga, personas mayores, épocas de exámenes, recuperación post operatoria.', price: 189  },
+      { id: 18, name: 'Miel de Alfalfa', details: 'Procedente de las flores de alfalfa. De color ambarino blanco, de buen sabor y propiedades similares a la miel de de trébol.', price: 165  }
     ];
     return {productos};
   }
--- src/app/producto.ts
+++ src/app/producto.ts
@@ -1,5 +1,7 @@
 export class Producto {
     id: number;
     name: string;
+    details: string;
+    price: number;
   }
   //tipo de datos que tendra la clase, se podra usar para validar formularios
